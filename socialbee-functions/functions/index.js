65cda0cfa7cfbbb4a75e17a4d4cefc071fc30346
viccycg <viccyg@digitalpharos.com>
2019-08-28 16:22:02 -0600

    Installed firebase for user auth. Created /signup route with email authentication which adds user and credentials.

--- socialbee-functions/functions/index.js
+++ socialbee-functions/functions/index.js
@@ -1,15 +1,28 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require ('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: "AIzaSyDt6CJlDIyjeceHwE98pLz2iyxN1zxMZUQ",
+    authDomain: "socialbee-c0ea7.firebaseapp.com",
+    databaseURL: "https://socialbee-c0ea7.firebaseio.com",
+    projectId: "socialbee-c0ea7",
+    storageBucket: "socialbee-c0ea7.appspot.com",
+    messagingSenderId: "328498940214",
+    appId: "1:328498940214:web:03f5ad7e710805c6"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+let token, userId;
 
 // using express to figure out the apis
 app.get('/buzzes', (req, res) => {
-    admin
-        .firestore()
+    db
         .collection('buzzes')
         .orderBy('createdAt', 'desc')
         .get()
@@ -35,7 +48,7 @@ app.post('/buzz', (req, res) => {
         createdAt: new Date().toISOString()
     };
 
-    admin.firestore()
+    db
         .collection('buzzes')
         .add(newBuzz)
         .then((doc) => {
@@ -47,42 +60,64 @@ app.post('/buzz', (req, res) => {
         })
 });
 
-// using firebase Cloud Functions - before express was installed
-// exports.getBuzzes = functions.https.onRequest((req, res) => {
-//   admin
-//     .firestore()
-//     .collection('buzzes')
-//     .get()
-//     .then((data) => {
-//       let buzzes = [];
-//       data.forEach((doc) => {
-//         buzzes.push(doc.data());
-//       });
-//       return res.json(buzzes);
-//     })
-//     .catch((err) => console.error(err));
-// });
-//
-// exports.createBuzz = functions.https.onRequest((req, res) => {
-//     if(req.method !== 'POST') {
-//         return res.status(400).json({ error: 'Method not allowed'});
-//     }
-//     const newBuzz = {
-//       body: req.body.body,
-//       userHandle: req.body.userHandle,
-//       createdAt: admin.firestore.Timestamp.fromDate(new Date())
-//     };
-//
-//   admin.firestore()
-//     .collection('buzzes')
-//     .add(newBuzz)
-//     .then((doc) => {
-//       res.json({ message: `document ${doc.id} created successfully` });
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ error: `something went wrong` });
-//       console.error(err);
-//     })
-// });
+//Signup route
+app.post('/signup', (req, res) => {
+   const newUser = {
+       email: req.body.email,
+       password: req.body.password,
+       confirmPassword: req.body.confirmPassword,
+       handle: req.body.handle,
+   };
+
+   // validate data
+   db.doc(`/users/${newUser.handle}`).get()
+       .then(doc => {
+           if (doc.exists) {
+               return res.status(400).json({ handle: `this handle is already taken`});
+           } else {
+               return firebase
+                   .auth()
+                   .createUserWithEmailAndPassword(newUser.email, newUser.password);
+           }
+       })
+       .then(data => {
+           userId = data.user.uid;
+           return data.user.getIdToken();
+       })
+       .then(idToken => {
+           token = idToken;
+           const userCredentials = {
+               handle: newUser.handle,
+               email: newUser.email,
+               createdAt: new Date().toISOString(),
+               userId
+           };
+           return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+       })
+       .then(() => {
+           return res.status(201).json({ token });
+       })
+       .catch(err => {
+           console.error(err);
+           if (err.code === "auth/email-already-in-use") {
+               return res.status(400).json({ email: `Email is already in use`});
+           } else {
+               return res.status(500).json({ error: err.code });
+           }
+       })
+
+   // firebase
+   //      .auth()
+   //      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+   //      .then((data) => {
+   //          return res
+   //              .status(201)
+   //              .json({ message: `user ${data.user.uid} signed up successfully` });
+   //      })
+   //      .catch((err) => {
+   //         console.error(err);
+   //         return res.status(500).json({ error: err.code });
+   //      });
+});
 
 exports.api = functions.https.onRequest(app);
