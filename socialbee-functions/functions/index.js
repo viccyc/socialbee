a76cb9d9624484bb454986cc4d49d4967e901707
viccycg <viccyg@digitalpharos.com>
2019-08-28 10:57:37 -0600

    Installed express and changed the functions to use express instead of google firebase cloud functions

--- socialbee-functions/functions/index.js
+++ socialbee-functions/functions/index.js
@@ -3,43 +3,85 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const express = require('express');
+const app = express();
+
+// using express to figure out the apis
+app.get('/buzzes', (req, res) => {
+    admin
+        .firestore()
+        .collection('buzzes')
+        .get()
+        .then((data) => {
+            let buzzes = [];
+            data.forEach((doc) => {
+                buzzes.push({
+                    buzzId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
+            });
+            return res.json(buzzes);
+        })
+        .catch((err) => console.error(err));
+});
+
+app.post('/buzz', (req, res) => {
+    const newBuzz = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore()
+        .collection('buzzes')
+        .add(newBuzz)
+        .then((doc) => {
+            res.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch((err) => {
+            res.status(500).json({ error: `something went wrong` });
+            console.error(err);
+        })
+});
+
+// using firebase Cloud Functions - before express was installed
+// exports.getBuzzes = functions.https.onRequest((req, res) => {
+//   admin
+//     .firestore()
+//     .collection('buzzes')
+//     .get()
+//     .then((data) => {
+//       let buzzes = [];
+//       data.forEach((doc) => {
+//         buzzes.push(doc.data());
+//       });
+//       return res.json(buzzes);
+//     })
+//     .catch((err) => console.error(err));
+// });
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello socialbee!");
-});
+// exports.createBuzz = functions.https.onRequest((req, res) => {
+//     if(req.method !== 'POST') {
+//         return res.status(400).json({ error: 'Method not allowed'});
+//     }
+//     const newBuzz = {
+//       body: req.body.body,
+//       userHandle: req.body.userHandle,
+//       createdAt: admin.firestore.Timestamp.fromDate(new Date())
+//     };
+//
+//   admin.firestore()
+//     .collection('buzzes')
+//     .add(newBuzz)
+//     .then((doc) => {
+//       res.json({ message: `document ${doc.id} created successfully` });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ error: `something went wrong` });
+//       console.error(err);
+//     })
+// });
 
-exports.getBuzzes = functions.https.onRequest((req, res) => {
-  admin
-    .firestore()
-    .collection('buzzes')
-    .get()
-    .then((data) => {
-      let buzzes = [];
-      data.forEach((doc) => {
-        buzzes.push(doc.data());
-      });
-      return res.json(buzzes);
-    })
-    .catch((err) => console.error(err));
-});
-
-exports.createBuzz = functions.https.onRequest((req, res) => {
-  const newBuzz = {
-    body: req.body.body,
-    userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
-  };
-
-  admin.firestore()
-    .collection('buzzes')
-    .add(newBuzz)
-    .then((doc) => {
-      res.json({ message: `document ${doc.id} created successfully` });
-    })
-    .catch((err) => {
-      res.status(500).json({ error: `something went wrong` });
-      console.error(err);
-    })
-});
+exports.api = functions.https.onRequest(app);
