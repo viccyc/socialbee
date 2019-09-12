38424afbe0619bd0b7b9d2ec4f57e524a4021b9a
viccycg <viccyg@digitalpharos.com>
2019-09-12 16:25:53 -0600

    Updated login page with email/password and styling

--- socialbee-client/src/pages/login.js
+++ socialbee-client/src/pages/login.js
@@ -1,15 +1,120 @@
 import React, {Component} from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import AppIcon from '../images/favicon.ico';
+import axios from 'axios';
+
+// MUI imports
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    },
+    image: {
+        zoom: '40%',
+        margin: '20px auto 20px auto'
+    },
+    pageTitle: {
+        margin: '10px auto 10px auto'
+    },
+    textField: {
+        margin: '10px auto 10px auto'
+    },
+    button: {
+        marginTop: 20
+    }
+};
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors: {}
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            loading: true
+        });
+        const userData = {
+            email: this.state.email,
+            passwod: this.state.password
+        };
+        axios.post('/login', userData)
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    loading: false
+                });
+                this.props.history.push('/');
+            })
+            .catch((err) => {
+                this.setState({
+                    error: err.response.data,
+                    loading: false
+                })
+            })
+    };
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
     render() {
+        const { classes } = this.props;
+        const { errors, loading } = this.state;
+
         return (
-            <div>
-                <h1>Login page</h1>
-            </div>
+            <Grid container className={classes.form}>
+                <Grid item sm/>
+                <Grid item sm>
+                    <img className={classes.image} src={ AppIcon } alt="app icon"/>
+                    <Typography variant="h2" className={classes.pageTitle}>
+                        Login
+                    </Typography>
+                    <form noValidate onSubmit={this.handleSubmit}>
+                        <TextField className={classes.textField}
+                            id="email" name="email"
+                            type="email" label="Email"
+                            value={this.state.email}
+                            helperText={errors.email}
+                            error={errors.email ? true : false}
+                            onChange={this.handleChange}
+                            fullWidth/>
+                        <TextField className={classes.textField}
+                            id="password" name="password"
+                            type="password" label="password"
+                            value={this.state.password}
+                            helperText={errors.password}
+                            error={errors.password ? true : false}
+                            onChange={this.handleChange}
+                            fullWidth/>
+                        <Button className={classes.button} type="submit"
+                                variant="contained" color="primary">
+                            Login
+                        </Button>
+                    </form>
+                </Grid>
+                <Grid item sm/>
+            </Grid>
         );
     }
 }
 
-export default Login;
+Login.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Login);
 
 
