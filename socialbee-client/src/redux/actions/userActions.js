0d014e0128f68f2256e8e37c53927e767c3e6fc1
viccycg <viccyg@digitalpharos.com>
2019-09-16 16:26:52 -0600

    Setup reducer types. Moved the axios code for login to userActions so that we can use dispatch

---
+++ socialbee-client/src/redux/actions/userActions.js
@@ -0,0 +1,21 @@
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI} from "../types";
+import axios from "axios";
+
+export const loginUser = (userData) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+    axios.post('/login', userData)
+        .then((res) => {
+            console.log(res.data);
+            localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
+            this.setState({
+                loading: false
+            });
+            this.props.history.push('/');
+        })
+        .catch((err) => {
+            this.setState({
+                errors: err.response.data,
+                loading: false
+            });
+        });
+};
