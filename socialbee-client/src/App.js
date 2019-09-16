dc78b32e11a2a3761b85f24e55c32bec9f2fe3d9
viccycg <viccyg@digitalpharos.com>
2019-09-16 15:58:29 -0600

    Initial setup of Redux and store with reducers

--- socialbee-client/src/App.js
+++ socialbee-client/src/App.js
@@ -5,6 +5,9 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import themeFile from './util/theme';
 import jwtDecode from 'jwt-decode';
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
 // Pages
 import home from './pages/home';
 import login from './pages/login';
@@ -21,7 +24,7 @@ if (token) {
     const decodedToken = jwtDecode(token);
     // if token has expired
     if (decodedToken.exp * 1000 < Date.now()) {
-        window.location.href = '/login'
+        window.location.href = '/login';
         authenticated = false;
     } else {
         authenticated = true;
