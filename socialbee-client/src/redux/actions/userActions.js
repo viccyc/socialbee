38f4ebad3c16cffa36a8cc6e2f5f11d31e9b894c
viccycg <viccyg@digitalpharos.com>
2019-09-17 09:20:56 -0600

    Updated user actions to use 'dispatch' for user data and errors

--- socialbee-client/src/redux/actions/userActions.js
+++ socialbee-client/src/redux/actions/userActions.js
@@ -1,21 +1,35 @@
 import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI} from "../types";
 import axios from "axios";
 
-export const loginUser = (userData) => (dispatch) => {
+export const loginUser = (userData, history) => (dispatch) => {
     dispatch({ type: LOADING_UI });
     axios.post('/login', userData)
         .then((res) => {
-            console.log(res.data);
-            localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-            this.setState({
-                loading: false
-            });
-            this.props.history.push('/');
+            const FBIdToken = `Bearer ${res.data.token}`;
+            localStorage.setItem('FBIdToken', FBIdToken);
+            // this will send the FBAuth token with every axios call
+            axios.defaults.headers.common['Authorization'] = FBIdToken;
+            dispatch(getUserData());
+            dispatch({ type: CLEAR_ERRORS });
+            history.push('/');
         })
         .catch((err) => {
-            this.setState({
-                errors: err.response.data,
-                loading: false
-            });
+            dispatch({
+                type: SET_ERRORS,
+                payload: err.response.data
+            })
         });
+};
+
+export const getUserData = () => (dispatch) => {
+    axios.get('/user')
+        .then(res => {
+            dispatch({
+                type: SET_USER,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            console.error(err);
+        })
 };
