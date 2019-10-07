70c36cae76233259acc6e77299ee9b43b88bbaca
viccycg <viccyg@digitalpharos.com>
2019-10-07 16:01:24 -0600

    Styled the skeleton cards

--- socialbee-client/src/util/BuzzSkeleton.js
+++ socialbee-client/src/util/BuzzSkeleton.js
@@ -7,9 +7,28 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 
-const styles = (theme) => {
-
-};
+const styles = (theme) => ({
+    ...theme.mainTheme,
+    card: {
+        display: 'flex',
+        marginBottom: 20
+    },
+    cardContent: {
+        width: '100%',
+        flexDirection: 'column',
+        padding: 25
+    },
+    cover: {
+        minWidth: 200,
+        objectFit: 'cover'
+    },
+    handle: {
+        width: 60,
+        height: 20,
+        backgroundColor: theme.palette.primary.main,
+        marginBottom: 7
+    }
+});
 
 const BuzzSkeleton = (props) => {
     const { classes } = props;
