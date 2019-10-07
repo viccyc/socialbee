43d6c5fda5ef733837139793c329931bbf76b59b
viccycg <viccyg@digitalpharos.com>
2019-10-07 16:16:46 -0600

    Further styling of skeleton card

--- socialbee-client/src/util/BuzzSkeleton.js
+++ socialbee-client/src/util/BuzzSkeleton.js
@@ -20,13 +20,32 @@ const styles = (theme) => ({
     },
     cover: {
         minWidth: 200,
-        objectFit: 'cover'
+        objectFit: 'cover',
+        opacity: 0.3
     },
     handle: {
         width: 60,
         height: 20,
         backgroundColor: theme.palette.primary.main,
         marginBottom: 7
+    },
+    date: {
+        height: 14,
+        width: 100,
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        marginBottom: 10
+    },
+    fullLine: {
+        height: 15,
+        width: '90%',
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        marginBottom: 10
+    },
+    halfLine: {
+        height: 15,
+        width: '50%',
+        backgroundColor: 'rgba(0,0,0,0.6)',
+        marginBottom: 10
     }
 });
 
